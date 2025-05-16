5d1b3cf2c6b5fecdd22a96a51d925516f63d79f0
James Flores <74877516+james-rod@users.noreply.github.com>
2025-05-15 20:55:07 -0400

    Find_Minimum_in_Rotated_Sorted_Array.js

--- Find_Minimum_in_Rotated_Sorted_Array.js
+++ Find_Minimum_in_Rotated_Sorted_Array.js
@@ -1,1 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0
+    let right = nums.length - 1
+    let result = nums[0]
 
+    while(left <= right){
+        if(nums[left] <= nums[right]){
+            result = Math.min(nums[left], result)
+            break
+        }
+
+
+
+        let middle = left + Math.floor((right - left) / 2)
+        result = Math.min(result, nums[middle])
+        if(nums[middle] >= nums[left]){
+            left = middle + 1
+        } else{
+            right = middle - 1
+        }
+    }
+
+    return result
+};
